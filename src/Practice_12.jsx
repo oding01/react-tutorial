4e0dd937c8a46c6069acd17522f8a39238f87799
이어진 <slimin92@naver.com>
2025-01-13 20:54:41 +0900

    ref와 state를 조합하여 회원가입 페이지를 만들고 유효성 검증을 위한 상태 단일화

---
+++ src/Practice_12.jsx
@@ -0,0 +1,84 @@
+import '@/App.css'
+import { useRef, useState } from 'react'
+
+// @ts-ignore
+function IdInput({ label, idRef }) {
+  const [required, setRequired] = useState(false)
+  return (
+    <div>
+      {label} :{' '}
+      <input
+        ref={idRef}
+        onChange={(e) => {
+          setRequired(e.currentTarget.value.length > 0)
+        }}
+      />
+      {required || <div style={{ color: 'red' }}>아이디를 입력해주세요.</div>}
+    </div>
+  )
+}
+
+// @ts-ignore
+function PasswordInput({ label, passwordRef }) {
+  const [valid, setValid] = useState({
+    maxLength: false,
+    minimumLength: false,
+    required: false,
+  })
+
+  function changeMode(e) {
+    if (e.currentTarget.innerText === '보여주기') {
+      passwordRef.current.type = 'text'
+      e.currentTarget.innerText = '감추기'
+    } else {
+      passwordRef.current.type = 'password'
+      e.currentTarget.innerText = '보여주기'
+    }
+  }
+  return (
+    <>
+      {label} :{' '}
+      <input
+        type='password'
+        ref={passwordRef}
+        onChange={(e) => {
+          setValid({
+            maxLength: e.currentTarget.value.length < 13,
+            minimumLength: e.currentTarget.value.length > 6,
+            required: e.currentTarget.value.length > 0,
+          })
+        }}
+      />
+      <button onClick={changeMode}>보여주기</button>
+      {valid.maxLength || <div style={{ color: 'red' }}>비밀번호는 최대 12자리입니다.</div>}
+      {valid.minimumLength || (
+        <div style={{ color: 'red' }}>비밀번호는 최소 6자리 이상이어야 합니다.</div>
+      )}
+      {valid.required || <div style={{ color: 'red' }}>비밀번호는 필수 입력값입니다.</div>}
+    </>
+  )
+}
+
+function Practice_12() {
+  const idRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  function register() {
+    console.log({})
+  }
+  console.log('- rerendered')
+
+  return (
+    <div>
+      <section style={{ textAlign: 'left' }}>
+        <IdInput label='아이디' idRef={idRef} />
+        <PasswordInput label='비밀번호' passwordRef={passwordRef} />
+        <div>
+          <button onClick={register}>회원가입</button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Practice_12
